5b2d55871e893242a865e34c51cee6feb2231d0a
harshasyam2 <122003191@sastra.ac.in>
2020-05-12 20:27:33 +0530

    Stylings are added

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,6 @@
 class Customer < ActiveRecord::Base
   validates :first_name, presence: true
   validates :email, presence: true
-  validates :password, presence: true
   has_secure_password
   has_many :orders
 
