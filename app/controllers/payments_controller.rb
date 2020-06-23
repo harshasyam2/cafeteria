95e93c9f19fdc11678f8c7b6159bef54776888a5
harshasyam2 <122003191@sastra.ac.in>
2020-06-23 13:34:38 +0530

    Stylings are added to order page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,28 +10,38 @@ class PaymentsController < ApplicationController
   def create
     cvv = params[:cvv]
     if cvv.to_i.to_s == cvv
-      new_payment = Payment.new(
-        order_id: params[:id],
-        cardnumber: params[:cardnumber],
-        cardexpiry: params[:cardexpiry],
-        cardholder: params[:cardholder],
-        cvv: params[:cvv],
-      )
-      if new_payment.save
-        order = Order.find(params[:id])
-        order.date = Date.today
-        order.status = "ordered"
-        if order.save
-          UserMailer.order_placed(order.id).deliver
-          flash[:alert] = "Your order confirmed"
-          redirect_to menus_path
+      if params[:cardexpiry] != "" and Date.parse(params[:cardexpiry]) >= Date.today
+        new_payment = Payment.new(
+          order_id: params[:id],
+          cardnumber: params[:cardnumber],
+          cardexpiry: params[:cardexpiry],
+          cardholder: params[:cardholder],
+          cvv: params[:cvv],
+        )
+        if new_payment.save
+          order = Order.find(params[:id])
+          order.date = Date.today
+          order.status = "ordered"
+          if order.save
+            UserMailer.order_placed(order.id).deliver
+            flash[:alert] = "Your order confirmed"
+            redirect_to menus_path
+          end
+        else
+          flash[:error_payments] = new_payment.errors.full_messages.join(",")
+          redirect_to orderitems_path
         end
       else
-        flash[:error_payments] = new_payment.errors.full_messages.join(",")
-        redirect_to orderitems_path
+        if params[:cardexpiry] == ""
+          flash[:error_payments] = "Please enter cardexpiry"
+          redirect_to orderitems_path
+        else
+          flash[:error_payments] = "Your Card expired.Please proceed with valid card"
+          redirect_to orderitems_path
+        end
       end
     else
-      flash[:error_payments] = new_payment.errors.full_messages.join(",")
+      flash[:error_payments] = "Invalid CVV.Please enter valid CVV"
       redirect_to orderitems_path
     end
   end
